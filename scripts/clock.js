bed133f62d35c88a796eacc251703fde70427de2
Xander Dheedene <xander.dheedene@ugent.be>
2023-06-07 23:19:40 +0200

    fixed centering for mobile

--- scripts/clock.js
+++ scripts/clock.js
@@ -7,12 +7,12 @@ function setDate() {
     const ww = window.innerWidth;
     const now = new Date();
     if (ww < 670) {
-        clock.textContent = `${leading0(now.getHours())} ${leading0(now.getMinutes())} ${leading0(now.getSeconds())}`;    
+        clock.innerHTML = `${leading0(now.getHours())}<br>${leading0(now.getMinutes())}<br>${leading0(now.getSeconds())}`;    
     }
     else {
-        clock.textContent = `${leading0(now.getHours())} : ${leading0(now.getMinutes())} : ${leading0(now.getSeconds())}`;
+        clock.innerHTML = `${leading0(now.getHours())} : ${leading0(now.getMinutes())} : ${leading0(now.getSeconds())}`;
     }
-    dayDate.textContent = `${weekday[now.getDay()]} ${leading0(now.getDate())}/${leading0(now.getMonth())}/${now.getFullYear()}`;
+    dayDate.innerHTML = `${weekday[now.getDay()]} ${leading0(now.getDate())}/${leading0(now.getMonth())}/${now.getFullYear()}`;
 }
 
 function leading0(number) {
